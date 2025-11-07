30a7e2d8254ffaae6390c3c310e4bc32ebfa11fb
nanditapanchal <nanditapanchalg@gmail.com>
2025-11-07 18:44:39 +0530

    Updated

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/upload', uploadRoutes);
